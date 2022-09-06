290d417841a166ff48de8fbf73c79efbec58f4c8
Banega00 <branislav.stojanovic@blinking.id>
2022-09-07 00:23:05 +0200

    fully decentralized p2p communication

--- client.js
+++ client.js
@@ -1,12 +1,19 @@
 const net = require('net')
 
+const PORT = 3001;
 
-let socket = net.connect(9000,'localhost');
+let socket = net.connect(9000,'localhost',()=>{
+    sendMessage(socket, {type:'port', port:PORT})
+});
 
-socket.on('data', data=>{
-    console.log(data.toString());
-})
+function sendMessage(socket, message){
+    socket.write(JSON.stringify(message));
+}
 
-process.stdin.on('data', data =>{
-    socket.write('data')
-})
+// socket.on('data', data=>{
+//     console.log(data.toString());
+// })
+
+// process.stdin.on('data', data =>{
+//     socket.write(data)
+// })
--- server.js
+++ server.js
@@ -1,19 +1,95 @@
 const net = require('net')
 
-const streams = [] //this is previously connected sockets
+const PORT = parseInt(process.argv[2])
+const ROOT_ADDRESS = '::ffff:127.0.0.1';
+const ROOT_PORT = 9001;
+
+let peers = [{address: ROOT_ADDRESS, port: ROOT_PORT}] //this is previously connected sockets
+
+//CLIENT PART
+let socket = net.connect(ROOT_PORT, ROOT_ADDRESS,()=>{
+    sendMessage(socket, {type:'port', port:PORT})
+
+    socket.on('data', data =>{
+        const message = JSON.parse(data.toString());
+        switch(message.type){
+            case 'peers':
+                peers = addMissingPeers(peers, message.peers)
+                peers.forEach(peer => {
+                    //becouse you are already connected to root server
+                    if (peer.address == ROOT_ADDRESS && peer.port == ROOT_PORT) return;
+                    
+                    connectToPeer(peer)
+                })
+                break;
+            case 'message':
+                console.log(message.message);
+                break;
+        }
+    })
+
+    process.stdin.on('data', data=>{
+        sendMessage(socket, {type:'message', message: data})
+    })
+})
+
+function addMissingPeers(oldPeers ,newPeers){
+    for(const newPeer of newPeers){
+        if(oldPeers.some(peer => newPeer.address == peer.address && newPeer.port == peer.port)) continue;
+
+        oldPeers.push(newPeer)
+    }
+    return oldPeers
+}
+
+function connectToPeer(peer){
+    console.log('Connecting to client',peer.port)
+    let socket = net.connect(peer.port, peer.address,()=>{
+        socket.on('data', data => {
+            const message = JSON.parse(data.toString());
+            switch (message.type) {
+                case 'message':
+                    console.log(message.message);
+                    break;
+            }
+        })
+
+        sendMessage(socket, {type:'peer-port', port: PORT})
+
+        process.stdin.on('data', data=>{
+            sendMessage(socket, {type:'message', message: data})
+        })
+    })
+}
+
+//SERVER PART
 const server = net.createServer(function (socket){//this is currently connected socket
     console.log('Client connected')
 
-    streams.forEach( otherSocket =>{
-        otherSocket.on('data', data => {
-            socket.write('data');
-        })
-        socket.on('data', data => {
-            otherSocket.write('data')
-        })
+    socket.on('data',data=>{
+
+        const message = JSON.parse(data.toString());
+        switch(message.type){
+            case 'port':
+                sendMessage(socket, {type:'peers', peers})
+                peers = addMissingPeers(peers, [{address: socket.remoteAddress, port: message.port}])
+                break;
+            case 'peer-port':
+                peers = addMissingPeers(peers, [{address: socket.remoteAddress, port: message.port}])
+                break;
+            case 'message':
+                console.log(message.message);
+                break;
+        }
     })
 
-    streams.push(socket)
+    process.stdin.on('data', data=>{
+        sendMessage(socket, {type:'message', message: data})
+    })
 })
 
-server.listen(9000)
+function sendMessage(socket, message){
+    socket.write(JSON.stringify(message));
+}
+
+server.listen(PORT)
