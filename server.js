32e187cc7a192540577fc51205ebfd8398985070
Banega00 <branislav.stojanovic@blinking.id>
2022-09-07 00:57:09 +0200

    disconnecting nodes

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const net = require('net')
 
 const PORT = parseInt(process.argv[2])
 const ROOT_ADDRESS = '::ffff:127.0.0.1';
-const ROOT_PORT = 9001;
+const ROOT_PORT = 9000;
+
+const randomString = (len) => {
+    return Math.random().toString(36).substring(2,len+2);
+  }
 
 let peers = [{address: ROOT_ADDRESS, port: ROOT_PORT}] //this is previously connected sockets
 
@@ -29,13 +33,13 @@ let socket = net.connect(ROOT_PORT, ROOT_ADDRESS,()=>{
     })
 
     process.stdin.on('data', data=>{
-        sendMessage(socket, {type:'message', message: data})
+        sendMessage(socket, {type:'message', message: data.toString()})
     })
 })
 
 function addMissingPeers(oldPeers ,newPeers){
     for(const newPeer of newPeers){
-        if(oldPeers.some(peer => newPeer.address == peer.address && newPeer.port == peer.port)) continue;
+        if(oldPeers.some(peer => newPeer.id == peer.id)) continue;
 
         oldPeers.push(newPeer)
     }
@@ -45,6 +49,8 @@ function addMissingPeers(oldPeers ,newPeers){
 function connectToPeer(peer){
     console.log('Connecting to client',peer.port)
     let socket = net.connect(peer.port, peer.address,()=>{
+        socket.id = randomString(10);
+        peer.id = socket.id;
         socket.on('data', data => {
             const message = JSON.parse(data.toString());
             switch (message.type) {
@@ -57,25 +63,36 @@ function connectToPeer(peer){
         sendMessage(socket, {type:'peer-port', port: PORT})
 
         process.stdin.on('data', data=>{
-            sendMessage(socket, {type:'message', message: data})
+            if(socket.destroyed) return;
+
+            sendMessage(socket, {type:'message', message: data.toString()})
+        })
+
+        socket.on('end', ()=>{
+            console.log("Removing", socket.id)
+            peers = peers.filter(peer => peer.id != socket.id)
         })
     })
 }
 
 //SERVER PART
 const server = net.createServer(function (socket){//this is currently connected socket
-    console.log('Client connected')
+    socket.id = randomString(10);
+    console.log('Client connected',socket.id)
 
-    socket.on('data',data=>{
+    socket.on('data', data=>{
 
         const message = JSON.parse(data.toString());
         switch(message.type){
             case 'port':
                 sendMessage(socket, {type:'peers', peers})
-                peers = addMissingPeers(peers, [{address: socket.remoteAddress, port: message.port}])
+                peers = addMissingPeers(peers, [{id: socket.id, address: socket.remoteAddress, port: message.port}])
                 break;
             case 'peer-port':
-                peers = addMissingPeers(peers, [{address: socket.remoteAddress, port: message.port}])
+                peers = addMissingPeers(peers, [{id: socket.id, address: socket.remoteAddress, port: message.port}])
+                break;
+            case 'disconnect':
+                peers = peers.filter(peer => peer.address != socket.remoteAddress || peer.port != message.port)
                 break;
             case 'message':
                 console.log(message.message);
@@ -83,13 +100,21 @@ const server = net.createServer(function (socket){//this is currently connected 
         }
     })
 
-    process.stdin.on('data', data=>{
-        sendMessage(socket, {type:'message', message: data})
+    process.stdin.on('data', data =>{
+        if(socket.destroyed) return;
+
+        sendMessage(socket, {type:'message', message: data.toString()})
+    })
+
+    socket.on('end', ()=>{
+        console.log("Removing",socket.id)
+        peers = peers.filter(peer => peer.id != socket.id)
+        socket.destroy()
     })
 })
 
 function sendMessage(socket, message){
-    socket.write(JSON.stringify(message));
+    socket.write(JSON.stringify(message))
 }
 
 server.listen(PORT)
